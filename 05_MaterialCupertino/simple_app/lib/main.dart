e0239fb04e65f83b0c09c7c16216edc8958c3fcf
shchurev.af <shchurev.af@ortus.ru>
2021-02-20 17:14:01 +1000

    First task

--- 05_MaterialCupertino/simple_app/lib/main.dart
+++ 05_MaterialCupertino/simple_app/lib/main.dart
@@ -28,22 +28,86 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+
+  final List<Map<String,dynamic>> bottomBarItems = [
+    {'title': 'Photo', 'icon': Icons.home},
+    {'title': 'Chat', 'icon': Icons.chat},
+    {'title': 'Albums', 'icon': Icons.album},
+  ];
+  bool _isPaid = false;
+  TabController _tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this, initialIndex: 0);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'simple app',
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Container(
+            child: Center(
+               child: _isPaid ? Text('Заказ успешно оплачен') : null,
             ),
-          ],
+          ),
+          Container(
+            color: Colors.purple,
+          ),
+          Container(
+            color: Colors.green,
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _tabController.index,
+        onTap: (int index) {
+          setState(() {
+            _tabController.index = index;
+          });
+        },
+        items: bottomBarItems.map((item) => BottomNavigationBarItem(icon: Icon(item['icon']), label: item['title'])).toList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(
+          Icons.add
         ),
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return Container(
+                height: 200,
+                color: Colors.white,
+                child: Column(
+                  children: <Widget>[
+                    ListTile(
+                      leading: const Icon(Icons.payment),
+                      title: const Text('Сумма'),
+                      trailing: const Text('200 руб'),
+                    ),
+                    RaisedButton(
+                      onPressed: () {
+                        setState(() {
+                          _isPaid = true;
+                        });
+                      },
+                      child: Text('Оплатить'),
+                    )
+                  ],
+                ),
+              );
+            }
+          );
+        },
       ),
     );
   }
