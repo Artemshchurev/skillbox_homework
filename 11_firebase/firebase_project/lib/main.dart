a2269e6e66d0d9a7f150b2d06a97af8429515edc
artemshchurev <artemshchurev@gmail.com>
2021-04-21 22:52:24 +1000

    Started working with firebase

--- 11_firebase/firebase_project/lib/main.dart
+++ 11_firebase/firebase_project/lib/main.dart
@@ -1,19 +1,49 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(),
+    final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: Text('Error occurred'),
+              ),
+            ),
+          );
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+            home: MyHomePage(),
+          );
+        }
+
+        return MaterialApp(
+          home: Scaffold(
+            body: CircularProgressIndicator(),
+          ),
+        );
+      },
     );
   }
 }
@@ -26,14 +56,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController _textController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  CollectionReference _todos = FirebaseFirestore.instance.collection('todos');
+
+  void _saveTodo() async {
+    DocumentReference ref = await _todos.add({
+      'title': _textController.text,
+      'isDone': false,
+    });
+    print(ref);
+  }
+
+  void _submit() {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      _saveTodo();
+      _formKey.currentState.reset();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
+      body: Form(
+        key: _formKey,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          children: [
+            TextFormField(
+              controller: _textController,
+              decoration: InputDecoration(
+                labelText: 'Новая Задача'
+              ),
+              validator: (value) {
+                return value == ''
+                    ? 'Введите текст'
+                    : null;
+              },
+            ),
+            ElevatedButton(
+              onPressed: _submit,
+              child: Text('Создать'),
+            )
+          ],
         ),
       ),
     );
